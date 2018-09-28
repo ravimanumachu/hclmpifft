5712dfe025914d93d9b02e2b2daf1cd3639021b0
Juan A. Rico <jarico@unex.es>
2018-09-28 11:04:23 +0200

    Second heterogeneous transpose implementation.
    - In place transpose.
    - Memory requirements:  Memory(matrix) + Max(chunk, P).
    - Datatypes requirements: 2 derived datatypes (excluded Complex). Type 2D to receive transposed data is Dynamically created in each transmission.
    - MPI_Communication: In each process -> for p in [0, P): MPI_Irecv + MPI_Send + MPI_Wait.
    - Efficiency: Lower than previous implementations (to be tested), however, memory requirements are lower).

--- tauloplib/mpicomm.c
+++ tauloplib/mpicomm.c
@@ -11,7 +11,7 @@
 
 
 
-static int HOM_transpose (const int p,
+static int HOM_transpose (const int P,
                           const int *rowd,
                           const int ngroups,
                           const int nthreadspergroup,
@@ -20,7 +20,7 @@ static int HOM_transpose (const int p,
                           fftw_complex* gMatrix);
 
 
-static int HET_transpose (const int p,
+static int HET_transpose (const int P,
                           const int *rowd,
                           const int ngroups,
                           const int nthreadspergroup,
@@ -33,6 +33,15 @@ static int HET_transpose (const int p,
 static int isHomogeneous (const int *rowd, const int P);
 
 
+
+
+static int HET_transpose_2 (const int P,
+                            const int *rowd,
+                            const int ngroups /* NOT USED */,
+                            const int nthreadspergroup,
+                            const int *rowdlocal /* NOT USED */,
+                            const int N,
+                            fftw_complex* gMatrix);
 
 /*----------------------------------------------------------------*/
 
@@ -127,11 +136,6 @@ static int HOM_transpose (const int p,
     // NOT IN PLACE:  MPI_Alltoall(sbuf, 1, MPI_TYPE_2D, rbuf, 1, MPI_TYPE_2D, MPI_COMM_WORLD);
     int rc = MPI_Alltoall(MPI_IN_PLACE, 1, MPI_TYPE_2D, gMatrix, 1, MPI_TYPE_2D, MPI_COMM_WORLD);
     
-    //int counts [4] = {1,1,1,1};
-    //int displs [4] = {0,1,2,3};
-    // NOT IN PLACE int rc = MPI_Alltoallv(sbuf, counts, displs, MPI_TYPE_2D, rbuf, counts, displs, MPI_TYPE_2D, MPI_COMM_WORLD);
-    //int rc = MPI_Alltoallv(MPI_IN_PLACE, counts, displs, MPI_TYPE_2D, gMatrix, counts, displs, MPI_TYPE_2D, MPI_COMM_WORLD);
-    
     if (rc != MPI_SUCCESS) {
         printf("[%d] Problems in MPI_Alltoall\n", rank);
     }
@@ -152,31 +156,18 @@ static int HOM_transpose (const int p,
 
 
 
-
-//  RICO:   TBD
-
-static int HET_transpose (const int p,
-                          const int *rowd,
-                          const int ngroups /* NOT USED */,
-                          const int nthreadspergroup,
-                          const int *rowdlocal /* NOT USED */,
-                          const int N,
-                          fftw_complex* gMatrix)
+static int HET_transpose (const int P,
+                            const int *rowd,
+                            const int ngroups /* NOT USED */,
+                            const int nthreadspergroup,
+                            const int *rowdlocal /* NOT USED */,
+                            const int N,
+                            fftw_complex* gMatrix)
 
 {
-    
     int  me;
-    int  P;
     
     MPI_Comm_rank(MPI_COMM_WORLD, &me);
-    MPI_Comm_size(MPI_COMM_WORLD, &P);
-    
-    /*
-     if (me == 0) {
-     fprintf(stderr, "Error: HETERGENEUOS transpose not fully supported yet (TEMPTATIVE IMPLEMENTATION.");
-     }
-     */
-    
     
     /* Complex type */
     int dsize = sizeof(fftw_complex);
@@ -246,33 +237,33 @@ static int HET_transpose (const int p,
     
     
     /* TEMPORAL: imprimir datos *
-    sleep(me);
-    
-    fprintf(stdout, "\n______  %d  _____", me);
-    fprintf(stdout, "\nScount:   ");
-    for (int p = 0; p < P; p++) {
-        fprintf(stdout, "%4d", scounts[p]);
-    }
-    fprintf(stdout, "\nRcount:   ");
-    for (int p = 0; p < P; p++) {
-        fprintf(stdout, "%4d", rcounts[p]);
-    }
-    fprintf(stdout, "\nSdispls:  ");
-    for (int p = 0; p < P; p++) {
-        fprintf(stdout, "%4d", sdispls[p]);
-    }
-    fprintf(stdout, "\nRdispls:  ");
-    for (int p = 0; p < P; p++) {
-        fprintf(stdout, "%4d", rdispls[p]);
-    }
-    fprintf(stdout, "\nbuf:  ");
-    for (int i = 0; i < tsize / cplx_ext; i++) {
-        fprintf(stdout, "%2.1f", (double)buf[i*2]);
-    }
-    fprintf(stdout, "\n");
-    
-    sleep(P - me);
-    */
+     sleep(me);
+     
+     fprintf(stdout, "\n______  %d  _____", me);
+     fprintf(stdout, "\nScount:   ");
+     for (int p = 0; p < P; p++) {
+     fprintf(stdout, "%4d", scounts[p]);
+     }
+     fprintf(stdout, "\nRcount:   ");
+     for (int p = 0; p < P; p++) {
+     fprintf(stdout, "%4d", rcounts[p]);
+     }
+     fprintf(stdout, "\nSdispls:  ");
+     for (int p = 0; p < P; p++) {
+     fprintf(stdout, "%4d", sdispls[p]);
+     }
+     fprintf(stdout, "\nRdispls:  ");
+     for (int p = 0; p < P; p++) {
+     fprintf(stdout, "%4d", rdispls[p]);
+     }
+     fprintf(stdout, "\nbuf:  ");
+     for (int i = 0; i < tsize / cplx_ext; i++) {
+     fprintf(stdout, "%2.1f", (double)buf[i*2]);
+     }
+     fprintf(stdout, "\n");
+     
+     sleep(P - me);
+     */
     
     
     
@@ -292,6 +283,111 @@ static int HET_transpose (const int p,
         MPI_Type_free(&MPI_TYPE_2D[p]);
     }
     
+    
+    return (EXIT_SUCCESS);
+}
+
+
+
+static int HET_transpose_2 (const int P,
+                          const int *rowd,
+                          const int ngroups /* NOT USED */,
+                          const int nthreadspergroup,
+                          const int *rowdlocal /* NOT USED */,
+                          const int N,
+                          fftw_complex* gMatrix)
+
+{
+    int  me;
+    
+    MPI_Comm_rank(MPI_COMM_WORLD, &me);
+    
+    /* Complex type */
+    int dsize = sizeof(fftw_complex);
+    
+    MPI_Datatype  MPI_FFTW_COMPLEX;
+    MPI_Type_contiguous(dsize, MPI_BYTE, &MPI_FFTW_COMPLEX);
+    MPI_Type_commit(&MPI_FFTW_COMPLEX);
+    
+    
+    /* Column type to pack data */
+    MPI_Datatype  MPI_TYPE_COLb, MPI_TYPE_COL;
+    
+    MPI_Aint cplx_lb, cplx_ext;
+    cplx_lb = 0;
+    MPI_Type_get_extent(MPI_FFTW_COMPLEX, &cplx_lb, &cplx_ext);
+    
+    MPI_Type_vector(rowd[me], 1, N, MPI_FFTW_COMPLEX, &MPI_TYPE_COLb);
+    MPI_Type_create_resized(MPI_TYPE_COLb, 0, cplx_ext, &MPI_TYPE_COL);
+    MPI_Type_commit(&MPI_TYPE_COL);
+    
+    
+    /* 2D types to receive data from each process */
+    int  scounts [P];
+    int  max_size = 0;
+    int  size;
+    
+    for (int p = 0; p < P; p++) {
+        
+        /* Size of the contiguous buffer to send */
+        MPI_Pack_size(rowd[p], MPI_TYPE_COL, MPI_COMM_WORLD, &size);
+        max_size = (size > max_size) ? size : max_size;
+        scounts[p] = size;
+    }
+    
+    char *buf = NULL;
+    
+    if ((buf = (char *) calloc (max_size, 1)) == NULL) {
+        fprintf(stderr, "ERROR: allocating memory for temporary buffers\n");
+        MPI_Abort(MPI_COMM_WORLD, (-1));
+    }
+    
+    int  pos = 0;
+    int  offset = 0;
+    
+    int  MY_ALLTOALL_TAG = 100;
+
+    
+    for (int p = 0; p < P; p++) {
+        
+        /* Type to receive */
+        MPI_Datatype  MPI_TYPE_2Db, MPI_TYPE_2D;
+        
+        MPI_Type_vector(rowd[me], rowd[p], N, MPI_FFTW_COMPLEX, &MPI_TYPE_2Db);
+        MPI_Type_create_resized(MPI_TYPE_2Db, 0, cplx_ext, &MPI_TYPE_2D);
+        MPI_Type_commit(&MPI_TYPE_2D);
+        
+        pos = 0;
+        MPI_Pack((char *)gMatrix + (offset * cplx_ext), rowd[p], MPI_TYPE_COL, buf, max_size, &pos, MPI_COMM_WORLD);
+        // TBI: Se pueden crear otro tipo 2D por columnas para para PACK, aunque no creo que mejore.
+        //MPI_Pack((char *)gMatrix + (offset * cplx_ext), 1, MPI_TYPE_2D_COL, buf, max_size, &pos, MPI_COMM_WORLD);
+        
+        if (p == me) {
+            
+            pos = 0;
+            MPI_Unpack(buf, max_size, &pos, (char *)gMatrix + (offset * cplx_ext), 1, MPI_TYPE_2D, MPI_COMM_WORLD);
+            
+        } else {
+            
+            MPI_Request req;
+            
+            MPI_Irecv((char *)gMatrix + (offset * cplx_ext), 1, MPI_TYPE_2D, p, MY_ALLTOALL_TAG, MPI_COMM_WORLD, &req);
+            MPI_Send(buf, scounts[p], MPI_PACKED, p, MY_ALLTOALL_TAG, MPI_COMM_WORLD);
+            MPI_Wait(&req, MPI_STATUS_IGNORE);
+            
+        }
+        
+        offset += rowd[p];
+        
+        MPI_Type_free(&MPI_TYPE_2Db);
+        MPI_Type_free(&MPI_TYPE_2D);
+    }
+    
+    free(buf);
+    
+    MPI_Type_free(&MPI_FFTW_COMPLEX);
+    MPI_Type_free(&MPI_TYPE_COLb);
+    MPI_Type_free(&MPI_TYPE_COL);
     
     return (EXIT_SUCCESS);
 }
